ff46bb451e14b02cfea1ba7280b9c45b71d9a3cf
FabianDi <fdiergardt@gmx.de>
2018-03-16 17:44:49 +0100

    First Version

--- real-hackers-flow/lib/logic.js
+++ real-hackers-flow/lib/logic.js
@@ -1,23 +1,33 @@
 'use strict';
 /**
- * Write your transction processor functions here
- */
-
-/**
- * Sample transaction
- * @param {org.real.hackers.ChangeAssetValue} changeAssetValue
+ * Sample transaction processor function.
+ * @param {org.acme.real.Validation} tx The sample transaction instance.
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.real.hackers.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
+
+function execValidation(validation) {
+    // Save the old value of the asset.
+    var oldValue = validation.application;
+    // Update the asset with the new value.
+    //tx.asset.value = validation.newValue;
+
+    // Get the asset registry for the asset.
+    return getAssetRegistry('org.acme.real.Application')
+        .then(function (assetRegistry) {
+
+            // Update the asset in the asset registry.
+            return assetRegistry.update(validation);
+
         })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
+        .then(function () {
+
+            // Emit an event for the modified asset.
+            //var event = getFactory().newEvent('org.acme.real', 'ApplicationSend');
+            //event.asset = tx.asset;
+            //event.oldValue = oldValue;
+            //event.newValue = tx.newValue;
+            //emit(event);
+
         });
+
 }
