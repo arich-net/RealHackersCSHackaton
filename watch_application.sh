bd525142da635b3269eba479e0ee81b94f188f4f
Ariel Vasquez <arich.net@gmail.com>
2018-04-08 17:53:17 +0200

    Testing javascript cookie verification

--- watch_application.sh
+++ watch_application.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #
-RESULT=$(curl -s --header "Content-Type: application/json" --header "Accept: application/json"  \
-    -XGET http://localhost:3000/api/Application/app-$1)
+TOKEN="EXSvdxD4Aodea014gatyHOXace3J6VGAstxfJ52AgL9RMHWidfCG6TVCsV1mdCif"
+RESULT=$(curl -s -k --header "Content-Type: application/json" \
+    --header "X-Access-Token: $TOKEN" \
+    --header "Accept: application/json"  \
+    -XGET https://blockchain.arich-net.com/api/Application/app-$1)
 
 echo $RESULT | jq .
 
