939f702767ad4b93df552e6fdbbd761ebd2e40d9
Ariel Vasquez <arich.net@gmail.com>
2018-06-13 22:38:56 +0200

    Update GIT verification script

--- scripts/verify_git_updates.sh
+++ scripts/verify_git_updates.sh
@@ -12,7 +12,8 @@ PIDFILE=${LOCKDIR}/PID
 
 log_message() {
    MESSAGE=$1
-   echo "[`date '+%Y%m%d %X'`][pid:`echo $$`][compver:`composer -v`] $MESSAGE"
+   USER=$2
+   echo "[`date '+%Y%m%d %X'`][pid:`echo $$`][compver:`composer -v`][user:$USER] $MESSAGE"
 }
 
 if mkdir "${LOCKDIR}" &>/dev/null; then
@@ -23,36 +24,37 @@ if mkdir "${LOCKDIR}" &>/dev/null; then
   git remote update &>/dev/null # Update REFS
   
   if [ "`git status -uno | egrep 'behind'`" != "" ]; then
-    log_message "GIT Repo require update"
+    GIT_USER=$(git log -n 1 | egrep -o '<.*>')
+    log_message "GIT Repo require update" "$GIT_USER"
     # We track the files that were pulled
     GIT_RESULT=$(git pull -v 2>/dev/null)
     if [ "$(echo $GIT_RESULT | egrep 'real-hackers-flow')" != "" ]; then
-      log_message "The model have been changed - need to recreate business network model"
+      log_message "The model have been changed - need to recreate business network model" "$GIT_USER"
       cd real-hackers-flow
-      log_message "Verify current deployed business network archive"
+      log_message "Verify current deployed business network archive" "$GIT_USER"
       TIMESTAMP=$(date +%s)
       composer network download -c admin@real-hackers-flow -a /tmp/tempbusiness_$TIMESTAMP.bna 2>&1
       DEPLOYED_BNA_VERSION=$(unzip -p /tmp/tempbusiness_$TIMESTAMP.bna package.json | jq .version | sed -e 's/\"//g')
       GITHUB_BNA_VERSION=$(jq .version < package.json | sed -e s/\"//g)
 
       if [ "$DEPLOYED_BNA_VERSION" == "$GITHUB_BNA_VERSION" ]; then
-        log_message "Deployed version the same as modified one - Please update package.json version"
+        log_message "Deployed version the same as modified one - Please update package.json version" "$GIT_USER"
       else
-        log_message "Create new BNA file definition with version $GITHUB_BNA_VERSION"
+        log_message "Create new BNA file definition with version $GITHUB_BNA_VERSION" "$GIT_USER"
         composer archive create --sourceType dir --sourceName .  2>&1
-        log_message "Install new BNA file definition with version $GITHUB_BNA_VERSION"
+        log_message "Install new BNA file definition with version $GITHUB_BNA_VERSION" "$GIT_USER"
         composer network install -a real-hackers-flow@$GITHUB_BNA_VERSION.bna -c PeerAdmin@real-hackers-network 2>&1
-        log_message "Upgrade to new business network version $GITHUB_BNA_VERSION"
+        log_message "Upgrade to new business network version $GITHUB_BNA_VERSION" "$GIT_USER"
         composer network upgrade -c PeerAdmin@real-hackers-network -n real-hackers-flow -V $GITHUB_BNA_VERSION 2>&1
         #composer archive create --sourceType dir --sourceName . -a real-hackers-flow@0.0.1.bna 2>&1
         #composer network update -a real-hackers-flow@0.0.1.bna -c admin@real-hackers-flow 2>&1        
-        log_message "Restarting REST server"
+        log_message "Restarting REST server" "$GIT_USER"
         docker stop rest 2>&1
         docker start rest 2>&1
-        log_message "The model have changed successfully"
+        log_message "The model have changed successfully" "$GIT_USER"
       fi
     fi
-    log_message "GIT Repo updated successfully"
+    log_message "GIT Repo updated successfully" "$GIT_USER"
   else
     log_message "GIT Repo DO NOT require update"
   fi
